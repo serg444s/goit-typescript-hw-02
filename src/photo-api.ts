c58902aff86056717152dfd6c1429b830c1aa14d
serg444s <143083342+serg444s@users.noreply.github.com>
2024-05-16 16:56:20 +0300

    upd

--- src/photo-api.ts
+++ src/photo-api.ts
@@ -8,5 +8,3 @@ export const fetchImages = async (page, query) => {
   );
   return response.data;
 };
-
-// https://api.unsplash.com/search/photos?client_id=azs8rW0W72OCT-Zd76iLD71_6ahhIu_39FvO7jKHhU0&page=1&query=cat
